5f0362b7c8fc8ddc7188659e1722af746490b77b
Asdfghjkkllljd <staricinal49@gmail.com>
2024-09-04 23:54:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
-CHANNEL_ID = 123123  # Айди канала
-INVOICE_LINK = "https://t.me/send?start=IVDPUVSBls87"  #  Линк на инвойс
-RULES_LINK = "https://t.me/c/2192323057/3"  #  Линк на правила
-NEWS_LINK = "https://t.me/+noTnZbQmYYhmMjIy"  #  Линк польз. соглашения
-SUPPORT_LINK = "https://t.me/starwebb"  #  Линк саппорта
-FAQ_LINK = "https://t.me/c/2192323057/3" # FAQ
-CRYPTO_PAY_TOKEN = "242947:"  #  Токен от CryptoPay фулл откат сделал, ща посмотрим
-BOT_TOKEN = "7404473505:-JcPSkOU8PA"  #  Токен бота че тупой, не я вставлял ранее, я говорю фулл скрипт откатитл, до изменений
+CHANNEL_ID = -1002220727686 # Айди канала
+INVOICE_LINK ="  t.me/send?start=IVa6rll05KWP #  Линк на инвойс
+RULES_LINK = "https://t.me/hammerbetw/9488"  #  Линк на правила
+NEWS_LINK = "https://t.me/hammer_nuws"  # https://telegra.ph/Polzavatelskoe-soglashenie-hammerbetcasino-08-31 Линк польз. соглашения
+SUPPORT_LINK = "https://t.me/mvd_want_me"  #  Линк саппорта
+FAQ_LINK = "https://t.me/hammerbetw/9488" # FAQ
+CRYPTO_PAY_TOKEN = "264106:AAWbEdJjab55z11qY4CDs6QfuHg4i4WYJEa"  #  Токен от CryptoPay фулл откат сделал, ща посмотрим
+BOT_TOKEN = "7367145825:AAFhljHI5GY6dZyeg6lcvGEzGH9YLALnDPs"  #  Токен бота че тупой, не я вставлял ранее, я говорю фулл скрипт откатитл, до изменений
 SCAM_SUM = 5
 SCAM_SUM_WHEEL = 10
 SCAM_WHEEL = 0
-ADMIN_ID = 5330676613  # Айди админа
-ADMIN_SD = 1452896288
-NEWS_ID = -1002192323057  # Айди канала новостей
-VIPLAT_ID = -1002059587052
+ADMIN_ID = 7292500266 # Айди админа
+ADMIN_SD = 7292500266
+NEWS_ID =  -1002181733344 # Айди канала новостей
+VIPLAT_ID = -1002220727686
 min_vivod = 1.1
 xamount_action = False
 kef = 2.8
@@ -28,9 +28,9 @@ kef_suefa = 2.5
 WIN_IMAGE = 'https://imgur.com/a/SIHUVBE'
 LOSE_IMG = 'https://imgur.com/a/hHbVtiR'
 CHAT_LINK = "https://t.me/+QDcTI221QRgwNzIy"
-CHAT_MESSAGE_INTERVAL = 1200
+CHAT_MESSAGE_INTERVAL = 12000000
 OFFLINE_DOLLAR = 92
-ERROR_CHAT_ID = -1002207071413
+ERROR_CHAT_ID = -1002220727686
 MAX_BET = 100
 
 bowling_values = {
